24dccf4b5f7565e1035423abb256b65cb9756521
mddct <hamddct@gmail.com>
2022-05-10 15:29:22 +0800

    compile init

--- src/ctcdecoder.cc
+++ src/ctcdecoder.cc
@@ -1,26 +1,35 @@
-#include <pybind11/pybind11.h>
-
 #include "decoder/ctc_prefix_beam_search.h"
+#include "decoder/search_interface.h"
+#include <pybind11/pybind11.h>
+#include <vector>
+
+struct DecodeResult {
+  std::vector<std::vector<int>> hypotheses;
+  std::vector<float> likelihood;
+
+  explicit DecodeResult();
+};
 
 namespace py = pybind11;
 
 // data shape: [max_time, bs, num_classes]
 // decoded shape: [bs, top_paths, max_time] ignore id: -1
 // log_probs shape: [bs, top_paths] ignore id: -1
-void ctc_beam_search_decoder(float *data, int max_time, int bs, int num_classes,
-                             int *sequence_length, int n_sequence_length,
-                             int beam_width, int top_path, int *decoded,
-                             float *log_probs) {
+std::vector<DecodeResult>
+ctc_beam_search_decoder(float *data, int max_time, int bs, int num_classes,
+                        int *sequence_length, int n_sequence_length,
+                        int beam_width, int top_path) {
+  std::vector<DecodeResult> results;
   if (data == nullptr) {
-    return;
+    return results;
   }
   if (max_time <= 0 || bs <= 0 || num_classes <= 0 || n_sequence_length <= 0 ||
       beam_width <= 0 || top_path <= 0) {
-    return;
+    return results;
   }
 
   wenet::CtcPrefixBeamSearchOptions opts{beam_width, top_path};
-  wenet::SearchInterface searcher;
+  std::unique_ptr<wenet::SearchInterface> searcher;
   searcher.reset(new wenet::CtcPrefixBeamSearch(opts));
 
   for (int i = 0; i < bs; i++) {
@@ -35,18 +44,34 @@ void ctc_beam_search_decoder(float *data, int max_time, int bs, int num_classes,
 
       ctc_log_probs.emplace_back(std::move(rows));
     }
-    searcher_->Search(ctc_log_probs);
-    const auto &hypotheses = searcher_->Inputs();
-    const auto &likelihood = searcher_->Likelihood();
+    searcher->Search(ctc_log_probs);
+    searcher->FinalizeSearch();
+    const auto &hypotheses = searcher->Inputs();
+    const auto &likelihood = searcher->Likelihood();
+
     // TODO: support export timestamp
     // fill timestamp
     // const auto &times = searcher_->Times();
 
-    // fill decoded
-    // fill log_probs
+    DecodeResult result;
+    for (auto i = 0; i < hypotheses.size(); i++) {
+      result.hypotheses.push_back(std::move(hypotheses[i]));
+    }
+    result.likelihood.resize(likelihood.size());
+    std::copy(likelihood.begin(), likelihood.end(), result.likelihood.begin());
+    results.emplace_back(std::move(result));
   }
+  return std::move(results);
 }
 
 PYBIND11_MODULE(_ctc_decoder, m) {
   m.doc() = "ctc decoder"; // optional module docstring
+
+  py::class_<DecodeResult>(m, "_DecodeResult")
+      .def(py::init<>())
+      .def_readwrite("hypotheses", &DecodeResult::hypotheses)
+      .def_readwrite("likelihood", &DecodeResult::likelihood);
+
+  m.def("ctc_beam_search_decoder", &ctc_beam_search_decoder,
+        py::return_value_policy::reference, "ctc prefix beam searcher decode");
 }
