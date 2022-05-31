f31665b7fd3cb285830883f8d6238a244bde31a6
Mddct <hamddct@gmail.com>
2022-05-31 16:13:13 +0800

    add FocalLoss

---
+++ py/focal_loss.py
@@ -0,0 +1,34 @@
+import torch
+
+class FocalLoss(torch.nn.Module):
+  
+    def __init__(
+        self,
+        alpha: float = 0.5,
+        gamma: float = 0.5,
+        stop_gradient_on_focal_loss_coefficient: bool = false,
+    ):
+        u""" [1] Focal loss https://arxiv.org/abs/1708.02002
+        Args:
+          stop_gradient_on_focal_loss_coefficient: If true, stops gradient on the
+          focal loss coefficient (1-p)^gamma to stabilize the gradient.
+        """
+       
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.stop_gradient_on_focal_loss_coefficient = stop_gradient_on_focal_loss_coefficient
+
+    def forward(self, logits: torch.Tensor) -> torch.Tensor:
+        """Calculate focal loss.
+        Args:
+            logits: [..., logit]
+        Returns:
+            loss[i..., j] = - αₜ(1-pₜ)ˠlog(pₜ)
+        """
+        probs = torch.exp(logits)
+        coefficient = torch.pow(1.0 - probs, self.gamma)
+        if self.stop_gradient_on_focal_loss_coefficient:
+          coefficient = coefficient.detach()
+        
+        return coefficient * logits
