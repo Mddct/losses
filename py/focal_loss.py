6103a052ca7f42dacbfcd3d4028131cbc533e4b6
Mddct <hamddct@gmail.com>
2022-06-02 10:44:52 +0800

    add SoftmaxCrossEntropyFocalLoss

--- py/focal_loss.py
+++ py/focal_loss.py
@@ -34,4 +34,37 @@ class SequenceFocalLoss(torch.nn.Module):
        
         return  -self.alpha * coefficient * logits
 
-  # TODO: class based focal losses
+class SoftmaxCrossEntropyFocalLoss(torch.nn.Module):
+    ''' (Note): This is the simple version of the implementation。 Not for class based just for sequence leval
+    '''
+    def __init__(
+        self,
+        alpha: float = 0.5,
+        gamma: float = 0.5,
+        stop_gradient_on_focal_loss_coefficient: bool = false,
+    ):
+          
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.stop_gradient_on_focal_loss_coefficient = stop_gradient_on_focal_loss_coefficient
+        self.softmax_cross_entropy_loss = torch.nn.NLLLoss(reduction="none")
+
+    def forward(self, logits: torch.Tensor, labels: torch.Tenor) -> torch.Tensor:
+        """Calculate focal loss.
+        (Note): using 
+        Args:
+            logits: logits before softmax because using nn.NLLLoss for numerically stable
+        Returns:
+            loss[i..., j] = - αₜ(1-pₜ)ˠlog(pₜ)
+        """
+        loss = self.softmax_cross_entropy_loss(logits, labels)
+        
+        # TODO: apply focal loss helper to resue code
+        probs = torch.exp(-loss)
+        coefficient = torch.pow(1.0 - probs, self.gamma)
+        if self.stop_gradient_on_focal_loss_coefficient:
+          coefficient = coefficient.detach()
+       
+        return  self.alpha * coefficient * loss
+
