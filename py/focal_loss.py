cfdf27cf64737baee4261f08a7956e9f913360d3
Mddct <hamddct@gmail.com>
2022-06-02 21:50:31 +0800

    add sigmod focal loss

--- py/focal_loss.py
+++ py/focal_loss.py
@@ -77,3 +77,39 @@ class SoftmaxCrossEntropyFocalLoss(torch.nn.Module):
        
         return  self.alpha * coefficient * loss
 
+class SigmodFocalLoss(torch.nn.Module):
+    u"""Focal loss for binary (sigmoid) logistic loss.
+    """
+    def __init__(
+        self,
+        alpha: float = 0.5,
+        gamma: float = 0.5,
+    ):
+          
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        
+    def forward(self, 
+                logits: torch.Tensor, 
+                labels: torch.Tenor,
+               ) -> torch.Tensor:
+        """Calculate binary focal loss
+        Args:
+            logits: logits before softmax because using nn.NLLLoss for numerically stable
+        Returns:
+            loss[i..., j] = - αₜ(1-pₜ)ˠlog(pₜ)
+        """
+        
+        loss = torch.nn.functional.binary_cross_entropy_with_logits(logits, labels, reduce=None)
+        if gamma is not None and gamma != 0:
+            # note:
+            # 1 logit[i] >= 0, (1 - p)ˠ = [1 - σ(x)]ˠ = [σ(-x)]ˠ
+            # 2 logit[i]< 0, pˠ = [σ(x)]
+            # than 1,2  and x:{0,1} -> 1-2x:{-1,0}
+            loss *= torch.pow(torch.sigmoid(logits * (1 - labels * 2)), gamma)
+        
+        if alpha is not None:
+            loss *= (alpha * labels + (1 - alpha) * (1 - labels))
+        return loss
+        
